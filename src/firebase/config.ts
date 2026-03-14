e78b97b262c21d44e7cc71dc360646b4a8556509
John Freeman <john@t3kniq.com>
2026-03-14 14:57:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3673070449-f277c",
+  "appId": "1:918212352467:web:3a704f0e02034084c83726",
+  "apiKey": "AIzaSyAuH64vGAgWc-t4YKe91df8GuJ7lfzU6oo",
+  "authDomain": "studio-3673070449-f277c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "918212352467"
+};
